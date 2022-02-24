379eaf0d54b072c7077d6156f099e90883f82402
gato365learningcenter <admin@gato365.com>
2022-02-24 19:29:47 +0000

    Fixed Retrieval Function

--- retrieving_data_for_general_data.R
+++ retrieving_data_for_general_data.R
@@ -27,21 +27,56 @@ get_df = function(url){
 
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ## Name: gather_data_web
-## Input: All urls and number of years on interest
+## Input: All urls and pn - player name
 ## Output: Data frame of all years of player of interest
 ## Purpose: Get Data frames from web
 ##~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-gather_data_web = function(imp_links,num_years){
+gather_data_web = function(imp_links,pn){
   full_data <- vector()
-  for(i in 1:num_years){
+  for(i in 1:length(imp_links)){
     ## Opens Access to website
     tab_df <- get_df(imp_links[i])
+    
+    ## Change column names vector
+    if(ncol(tab_df) == 30){
+    
+    
+    app_cn_vec = c("Rk","G","Date","Age","Tm",
+                   "Game_Location","Opp","Game_Outcome",     
+                   "GS","MP","FG","FGA","FG_Percent","3P",
+                   "3PA","3P_Percent","FT","FTA","FT_Percent",
+                   "ORB","DRB","TRB","AST","STL","BLK","TOV",
+                   "PF","PTS","GmSc","Plus_Minus")
+    colnames(tab_df) = app_cn_vec
+    } else if(ncol(tab_df) == 29) {
+      
+      app_cn_vec = c("Rk","G","Date","Age","Tm",
+                     "Game_Location","Opp","Game_Outcome",     
+                     "GS","MP","FG","FGA","FG_Percent","3P",
+                     "3PA","3P_Percent","FT","FTA","FT_Percent",
+                     "ORB","DRB","TRB","AST","STL","BLK","TOV",
+                     "PF","PTS","GmSc")
+      colnames(tab_df) = app_cn_vec
+      tab_df$Plus_Minus = NA
+      
+    }
+    ## Add Season Variable
     tab_df$Season <- paste0("season_",i,sep='')
     ## Joins DF
     full_data <- rbind(full_data,tab_df)  
     
     cat(paste0("season_",i,sep=''),'\n')
   }
+  
+  ## Add Player Name Variable and Make important Changes
+  full_data = full_data %>%
+    filter(Rk != "Rk") %>% 
+    mutate(name = pn,
+           Game_Location = ifelse(Game_Location == "@","Away","Home"),
+           Point_Margin = as.numeric(str_remove_all(string = Game_Outcome,"\\(|\\)|W|L|\\+| "),
+           Game_Outcome = ifelse(str_detect(string = Game_Outcome, pattern = "W"),"W","L")
+           ))
+  full_data$number_game = 1:nrow(full_data)
   return(full_data)
 }
 
@@ -65,136 +100,44 @@ num_years = 15
 
 ## Create a list of urls for each player
 mj_years = 1985:2003
+## Removing years at which MJ is not in NBA
 mj_years = mj_years[ !mj_years %in% c(1994,1999,2000,2001)]
 
+
+## Gather Data for MJ
 info_mj  = paste("http://www.basketball-reference.com/players/j/jordami01/gamelog/")
 mj_links = paste(rep(info_mj,num_years),mj_years,"/",sep="")
+mj_data = gather_data_web(mj_links,'MJ')
 
-
-
-
-
-
-
-
-
-## Removing years at which MJ is not in NBA
-
+## Gather Data for KB
 info_kb = paste("http://www.basketball-reference.com/players/b/bryanko01/gamelog/")
 kb_links = paste(rep(info_kb,num_years),seq(kb_begin_year,kb_begin_year+(num_years-1)),"/",sep="")
+kb_data = gather_data_web(kb_links,'KB')
 
+## Gather Data for LJ
 info_lj = paste("http://www.basketball-reference.com/players/j/jamesle01/gamelog/")
 lj_links = paste(rep(info_lj,num_years),seq(lj_begin_year,lj_begin_year+(num_years-1)),"/",sep="")
-
-
-
-
-
-##--------------------
-## Step 2a: Get LeBron James Data
-##--------------------
-
-gather_data_web <- function(imp_links,player_initial){
-  ## Gathering Data Using Functions
-  lst = map(imp_links,get_df)
-  
-  ## Change Names of element of list to be season
-  names(lst) = paste0('season_',1:15)
-  
-  
-  ## Change column names vector
-  app_cn_vec = c("Rk","G","Date","Age","Tm",
-                 "Game_Location","Opp","Game_Outcome",     
-                 "GS","MP","FG","FGA","FG_Percent","3P",
-                 "3PA","3P_Percent","FT","FTA","FT_Percent",
-                 "ORB","DRB","TRB","AST","STL","BLK","TOV",
-                 "PF","PTS","GmSc","Plus_Minus")
-  lst <- lapply(lst, setNames, app_cn_vec)
-  
-  
-  ## Place a column in each data frame that correspond to name
-  lst = map2(lst, names_seasons, ~.x %>% mutate(Season = .y))
-  ## Turn list into 1 data frame
-  df = bind_rows(lst) %>% 
-    mutate(Player_Name = player_initial)
-  
-  return(df)
-}
-
-
-
-
-##--------------------
-## Step 2b: Get Michael Jordan Data
-##--------------------
-
-## Gathering Data Using Functions
-mj_lst = map(mj_links,get_df)
-
-
-
-
-
-
-mj_data = gather_data_web(mj_links,'MJ')
 lj_data = gather_data_web(lj_links,'LJ')
 
-## First 4 years of data for Kobe
-kb_data = gather_data_web(kb_links,4)
-
-## KB Cleaning, new format for years 5 and on
-n_seasons = 11
-kb_data_new = vector()
-for(i in 1:n_seasons){
-  kb_data_tmp = get_df(kb_links[4+i])
-  kb_data_tmp = kb_data_tmp[,-30]; 
-  kb_data_tmp$Season <- paste0("season_",4+i,sep='')
-  kb_data_new = rbind(kb_data_new,kb_data_tmp)
-}
-
-colnames(kb_data_new) = colnames(kb_data)
-kb_data = rbind(kb_data,kb_data_new)
 
 
 
 
 
 
-## Create Variable for home or away
-mj_data$Game_Location = ifelse(mj_data[,6] == "@","Away","Home")
-kb_data$Game_Location = ifelse(kb_data[,6] == "@","Away","Home")
-lj_data$Game_Location = ifelse(lj_data[,6] == "@","Away","Home")
-
-
-
-
-## Create Variable for win or lose
-mj_data$Game_Outcome = ifelse(str_detect(string = mj_data[,8], pattern = "W"),"W","L")
-kb_data$Game_Outcome = ifelse(str_detect(string = kb_data[,8], pattern = "W"),"W","L")
-lj_data$Game_Outcome = ifelse(str_detect(string = lj_data[,8], pattern = "W"),"W","L")
-
-
-
-## Create Variable for point margin
-mj_data$Point_Margin = as.numeric(str_remove_all(string = mj_data[,8],"\\(|\\)|W|L|\\+| "))
-kb_data$Point_Margin = as.numeric(str_remove_all(string = kb_data[,8],"\\(|\\)|W|L|\\+| "))
-lj_data$Point_Margin = as.numeric(str_remove_all(string = lj_data[,8],"\\(|\\)|W|L|\\+| "))
 
 
 
 
 
 
-## Remove columns that are not important and add a label for players
-mj_data = mj_data[,-c(6,8)]; mj_data$Name = "MJ"
-kb_data = kb_data[,-c(6,8)];  kb_data$Name = "KB"
-lj_data = lj_data[,-c(6,8,30)] ; lj_data$Name = "LJ"
 
 
 
-mj_data$number_game = 1:nrow(mj_data)
-lj_data$number_game = 1:nrow(lj_data)
-kb_data$number_game = 1:nrow(kb_data)
+
+
+
+
 
 
 
